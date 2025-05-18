40da2869cfe17c6c3e2db1882d1feba9ba4d0b20
Devxnsh23 <devanshsinghlmfao@gmail.com>
2025-05-18 22:23:34 +0530

    Initial commit

---
+++ save_note.php
@@ -0,0 +1,14 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "goldengown");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $orderId = $_POST['orderId'];
+    $note = mysqli_real_escape_string($conn, $_POST['note']);
+
+    $query = "UPDATE orders SET notes = '$note' WHERE orderId = '$orderId'";
+    mysqli_query($conn, $query);
+}
+
+header("Location: dashboard_d.php");
+exit;
+?>
